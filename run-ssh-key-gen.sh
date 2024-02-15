3a0aa17b523317116d66ef6adad7d0a65e55226e
braucktoon <braucktoon@gmail.com>
2024-02-14 20:18:19 -0500

    bug fixes

--- run-ssh-key-gen.sh
+++ run-ssh-key-gen.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #run ssh-key-gen
-ansible-playbook site.yml -t ssh-key-gen -i inventory --vault-password-file ~/vaultpw --ask-become-pass
+ansible-playbook site.yml -t ssh-key-gen,ssh-config -i inventory --vault-password-file ~/vaultpw --ask-become-pass
